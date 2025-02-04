7be9ef68086ff1dce9014783b51513554f8cb3b8
Claudia Morin <92906339+ClaudiaMorin@users.noreply.github.com>
2025-02-03 16:36:06 -0800

    Updated to test more easily for unique family members
    Added an additional dictionary of sets.

--- ExpeditorsQuestion.java
+++ ExpeditorsQuestion.java
@@ -3,18 +3,30 @@ package ExpeditorsDITQuestion;
 
 import java.util.*;
 
+/*
+In answering this question I attempted to keep the code flexible for more than the original intent of simply processing
+a given data file once, what if there are multiple files or I am asked to extend this to users who are adding more households or people??
+To this end I kept three dictionaries because I needed to maintain a set of both the members of a unique family
+and the uniqueness of each address.  Testing for the uniqueness of an address was fairly simple as I just checked the dictionary keys as they
+have to be unique by definition.  Uniqueness of people within an address was more of a trade-off between creating an "Easy-to-sort-print" arraylist of String[]
+or using a Set<String> that could be used to insure uniqueness within an address.  I opted to keep both.  More data storage but less repeated processing...
+ */
 public class ExpeditorsQuestion {
-    public final Dictionary<String, ArrayList<String[]>> familyMap;
-    public final Dictionary<Integer, String> familyMapToGroupID;
+    //Keeps member data as strings so that the test for uniqueness can happen easily
+    private final Dictionary<String, Set<String>> familySets;
+    //Designed to allow simple data storage for sorting and printing
+    public final Dictionary<String, ArrayList<String[]>> familyMaptoData;
+    public final Dictionary<Integer, String> GroupIDMappingFamilyAddress;
     private DataInputOutFile fileProcessor;
     ExpeditorsQuestion()
     {
-        familyMap = new Hashtable<>();
-        familyMapToGroupID = new Hashtable<>();
+        familySets = new Hashtable<>();
+        GroupIDMappingFamilyAddress = new Hashtable<>();
+        familyMaptoData = new Hashtable<>();
     }
 
     //using dictionary.
-    public void processProfile(String textFileIn, String textFileOut)
+    public void processProfileFile(String textFileIn, String textFileOut)
     {
         fileProcessor = new DataInputOutFile(textFileIn, textFileOut);
         ArrayList<String> entry = fileProcessor.readFile();
@@ -24,25 +36,23 @@ public class ExpeditorsQuestion {
             if(entryCleaned != null)
             {
                 String address = entryCleaned[0];
-                String[] newMember = new String[]{entryCleaned[1], entryCleaned[2], entryCleaned[3]};
-                if(familyMap.get(address) == null)
-                {
-                    ArrayList<String[]> newPerson = new ArrayList<>();
-                    newPerson.add(newMember);
-                    familyMap.put(address, newPerson);
-                    familyMapToGroupID.put(familyMapToGroupID.size(), address);
+                String newMember = entryCleaned[1];
+                //if the family map doesn't exist we need to create it.
+                if(familySets.get(address) == null ) {
+                    GroupIDMappingFamilyAddress.put(GroupIDMappingFamilyAddress.size(), address);
+                    Set<String> newSet = new HashSet<>();
+                    familySets.put(address, newSet);
                 }
-                else //family address already exists!
-                {
-                    familyMap.get(address).add(newMember);
-                }
+                //now add new member!  If this isn't a unique member the Set will fail to add!
+                familySets.get(address).add(newMember);
             }
         }
-        removeDups();
+        createFamilyMapData();
         sortArraysLastFirst();
     }
 
-    //All the string manipulation and removal of white spaces goes here.
+    //All the string manipulation and removal of white spaces.  The array that is returned contains the address in
+    //String[0] and the Member in String[1] for ease of extraction
     private  String[] cleanEntryString(String entry)
     {
         String[] profile = entry.replaceAll("\"", "").split(",");
@@ -56,35 +66,25 @@ public class ExpeditorsQuestion {
 
         String firstName = profile[0].substring(0, 1).toUpperCase() + profile[0].toLowerCase().substring(1).stripLeading().trim();
         String lastName = profile[1].substring(0, 1).toUpperCase() + profile[1].toLowerCase().substring(1).stripLeading().trim();
-        String[] entryCleaned = new String[]{ address, firstName, lastName, profile[5].trim()};
+        String[] entryCleaned = {address, firstName + " " + lastName + " " + profile[5].trim()};
         return entryCleaned;
     }
 
-    /* Make sure we aren't adding mutiples of the same person */
-    //Need to convert the String[] to a simple String so that I can add them to the hashset and the
-    //dups will be dumped, a String[] is seen as a unique object so the set conversion won't work as well
-    //This might be a costly way to remove dups but it is better than doing a loop through everytime someone new is added!
-    private void removeDups()
+    //Designed to convert the maps of unique members into datasets that are more easily sorted, printed, etc
+    private void createFamilyMapData()
     {
-        for(int count = 0; count < familyMapToGroupID.size(); count++) {
-            String addressAsKey = familyMapToGroupID.get(count);
-            Set<String> noDups = new HashSet<>();
-            if(familyMap.get(addressAsKey).size() > 1)
-            {
-                for(int i = 0; i < familyMap.get(addressAsKey).size(); i++)
-                {
-                    String temp = String.join( ",", familyMap.get(addressAsKey).get(i));
-                    noDups.add(temp);
-                }
 
-                familyMap.get(addressAsKey).clear();
-                List<String> cleanList = new ArrayList<>(noDups);
-                for(int i = 0; i < cleanList.size(); i++)
-                {
-                    String[] temp = cleanList.get(i).split(",");
-                    familyMap.get(addressAsKey).add(temp);
-                }
+        for(int familyID = 0; familyID <  GroupIDMappingFamilyAddress.size(); familyID++)
+        {
+            String address = GroupIDMappingFamilyAddress.get(familyID);
+            ArrayList<String[]> buildFamily = new ArrayList<>();
+            Set<String> curFamily = familySets.get(address);
+            for(String member : curFamily)
+            {
+                String[] curr = member.split(" ");
+                buildFamily.add(curr);
             }
+            familyMaptoData.put(address, buildFamily);
         }
     }
 
@@ -96,8 +96,8 @@ public class ExpeditorsQuestion {
         Comparator<String[]> compAge = (s1, s2) -> Integer.compare(Integer.parseInt(s2[2]), Integer.parseInt(s1[2]));
         Comparator<String[]> compByName = compLastName.thenComparing(compFirstName).thenComparing(compAge);
 
-        for(int count = 0; count < familyMapToGroupID.size(); count++) {
-            ArrayList<String[]> family = familyMap.get(familyMapToGroupID.get(count));
+        for(int count = 0; count < GroupIDMappingFamilyAddress.size(); count++) {
+            ArrayList<String[]> family = familyMaptoData.get(GroupIDMappingFamilyAddress.get(count));
 
             if(family.size() > 1)
             {
@@ -126,9 +126,9 @@ public class ExpeditorsQuestion {
     {
         ArrayList<String> retArrayList = new ArrayList<>();
 
-        for(int count = 0; count < familyMapToGroupID.size(); count++)
+        for(int count = 0; count < GroupIDMappingFamilyAddress.size(); count++)
         {
-            ArrayList<String[]> family = familyMap.get(familyMapToGroupID.get(count));
+            ArrayList<String[]> family = familyMaptoData.get(GroupIDMappingFamilyAddress.get(count));
             String address = "GroupID: " + count + "  Occupant(s): " + String.valueOf(family.size());
             boolean found = false;
             for(String[] member : family)
@@ -141,7 +141,7 @@ public class ExpeditorsQuestion {
                        retArrayList.add(address);
                        found = true;
                     }
-                    retArrayList.add("     " + member[0] + ", " + member[1] + ", " + familyMapToGroupID.get(count) + ", " + member[2]  );
+                    retArrayList.add("     " + member[0] + ", " + member[1] + ", " + GroupIDMappingFamilyAddress.get(count) + ", " + member[2]  );
                 }
             }
         }
